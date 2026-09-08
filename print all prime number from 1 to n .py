31783e2a8c1d0e5482b03b0cb0949d98e5b659b7
zainuddinahmed937-cpu <zainuddinahmed937@gmail.com>
2026-09-08 22:04:56 +0530

    Create print all prime number from 1 to n .py

---
+++ print all prime number from 1 to n .py
@@ -0,0 +1,12 @@
+n = int(input("Enter N: "))
+
+for num in range(2, n + 1):
+    prime = True
+
+    for i in range(2, num):
+        if num % i == 0:
+            prime = False
+            break
+
+    if prime:
+        print(num)
