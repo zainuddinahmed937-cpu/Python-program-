89028e15c214064149590847644d6c799bfe371a
zainuddinahmed937-cpu <zainuddinahmed937@gmail.com>
2026-09-08 21:29:48 +0530

    Create marks.py

---
+++ marks.py
@@ -0,0 +1,12 @@
+marks = int(input("Enter marks: "))
+
+if marks >= 90:
+    print("Grade A")
+elif marks >= 80:
+    print("Grade B")
+elif marks >= 70:
+    print("Grade C")
+elif marks >= 60:
+    print("Grade D")
+else:
+    print("Grade F")
