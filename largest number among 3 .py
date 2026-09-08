bf1d8fd6dfc50fbef74cd1011c2300dcb81d88b0
zainuddinahmed937-cpu <zainuddinahmed937@gmail.com>
2026-09-08 21:20:38 +0530

    Create largest number among 3 .py

---
+++ largest number among 3 .py
@@ -0,0 +1,10 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+if a > b and a > c:
+    print("Largest =", a)
+elif b > c:
+    print("Largest =", b)
+else:
+    print("Largest =", c)
