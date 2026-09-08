9783bb69ba8b7587324948f13599283ee0a24205
zainuddinahmed937-cpu <zainuddinahmed937@gmail.com>
2026-09-08 21:31:55 +0530

    Create electricity bill.py

---
+++ electricity bill.py
@@ -0,0 +1,10 @@
+units = int(input("Enter units: "))
+
+if units <= 100:
+    bill = units * 2
+elif units <= 200:
+    bill = 100 * 2 + (units - 100) * 3
+else:
+    bill = 100 * 2 + 100 * 3 + (units - 200) * 5
+
+print("Electricity Bill =", bill)
