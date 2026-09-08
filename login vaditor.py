fc86637bc40672d4e0e04f80b796e882c51da262
zainuddinahmed937-cpu <zainuddinahmed937@gmail.com>
2026-09-08 21:28:29 +0530

    Create login vaditor.py

---
+++ login vaditor.py
@@ -0,0 +1,7 @@
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+if username == "admin" and password == "1234":
+    print("Login successful")
+else:
+    print("Invalid username or password")
