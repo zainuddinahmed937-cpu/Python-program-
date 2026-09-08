c7eed8d9ff927e5717dc00c31c8ec5ef6b7879bf
zainuddinahmed937-cpu <zainuddinahmed937@gmail.com>
2026-09-08 21:46:48 +0530

    Create quadrant.py

---
+++ quadrant.py
@@ -0,0 +1,13 @@
+x = int(input("Enter x: "))
+y = int(input("Enter y: "))
+
+if x > 0 and y > 0:
+    print("First Quadrant")
+elif x < 0 and y > 0:
+    print("Second Quadrant")
+elif x < 0 and y < 0:
+    print("Third Quadrant")
+elif x > 0 and y < 0:
+    print("Fourth Quadrant")
+else:
+    print("Point lies on an axis")
