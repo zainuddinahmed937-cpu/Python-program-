638f68c76132965d6a52b2a284be281bc4c7ac46
zainuddinahmed937-cpu <zainuddinahmed937@gmail.com>
2026-09-08 20:57:06 +0530

    Create checking all three number .py

---
+++ checking all three number .py
@@ -0,0 +1,8 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+if a == b and b == c:
+    print("All three are equal")
+else:
+    print("Not equal")
