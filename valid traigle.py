822b08474023b150756fbd1453bd6e2db46ed8c8
zainuddinahmed937-cpu <zainuddinahmed937@gmail.com>
2026-09-08 21:34:31 +0530

    Create valid traigle.py

---
+++ valid traigle.py
@@ -0,0 +1,8 @@
+a = int(input("Enter first side: "))
+b = int(input("Enter second side: "))
+c = int(input("Enter third side: "))
+
+if a + b > c and a + c > b and b + c > a:
+    print("Valid triangle")
+else:
+    print("Invalid triangle")
