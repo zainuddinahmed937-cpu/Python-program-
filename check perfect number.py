eafd1ed0d19d00ad1f753fdd9ab8c2387ff8beb5
zainuddinahmed937-cpu <zainuddinahmed937@gmail.com>
2026-09-08 22:18:06 +0530

    Create check perfect number.py

---
+++ check perfect number.py
@@ -0,0 +1,11 @@
+n = int(input("Enter a number: "))
+sum = 0
+
+for i in range(1, n):
+    if n % i == 0:
+        sum += i
+
+if sum == n:
+    print("Perfect number")
+else:
+    print("Not a perfect number")
