5c72fc3e8958d397e19b3c274a2956637c67dd89
zainuddinahmed937-cpu <zainuddinahmed937@gmail.com>
2026-09-08 20:50:53 +0530

    Create greater number .py

---
+++ greater number .py
@@ -0,0 +1,9 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+if a > b:
+    print("First number is greater")
+elif b > a:
+    print("Second number is greater")
+else:
+    print("Both are equal")
